5d905cf9dd4845dd0d0f6e99b3b25ff4aad4053c
Olga Gladkova <olga_gladkova@tut.by>
2021-03-25 13:49:04 +0300

    message update

--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -12,7 +12,7 @@ function Message(props: MessageType) {
 
         <div className={s.message}>
             <div>
-                {props.message}
+                {props.message+"zzzzzz"}
             </div>
             {/*<div>*/}
             {/*<textarea ref = {addMessage}></textarea>*/}
