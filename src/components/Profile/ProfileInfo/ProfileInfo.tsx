9d7192cd43adb2fc7e591c97eae7ac7118d48050
Lisla Fox <lisla@Lislas-MBP.home>
2021-08-19 13:19:29 +0300

    lesson 84

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,11 +3,14 @@ import s from './ProfileInfo.module.css';
 import Preloader from "../../common/preloader/Preloader";
 import {ProfileUserType} from "../../../Redux/profileReducer";
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+
 type PropsType = {
     profile: null | ProfileUserType
     status: string
     updateUserStatus: (status: string) => any
 }
+
 function ProfileInfo(props: PropsType) {
     if (!props.profile) {
         return <Preloader/>
@@ -19,12 +22,15 @@ function ProfileInfo(props: PropsType) {
             {/*        src='https://kids.sandiegozoo.org/sites/default/files/2019-01/animal-hero-arcticfoxe.jpg'/>*/}
             {/*</div>*/}
             <div className={s.descriptionBlock}>
-                <img src={props.profile.photos.large ? props.profile.photos.large: ''}/>
+                <img src={props.profile.photos.large ? props.profile.photos.large : ''}/>
                 {props.profile.fullName}
                 {props.profile.aboutMe}
-                <ProfileStatus status={props.status} updateUserStatus={props.updateUserStatus}/>
+                <ProfileStatusWithHooks status={props.status} updateUserStatus={props.updateUserStatus}/>
+                {/*With classComponent*/}
+                {/*<ProfileStatus status={props.status} updateUserStatus={props.updateUserStatus} />*/}
             </div>
         </div>
     )
 }
+
 export default ProfileInfo;
