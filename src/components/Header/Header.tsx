793ba40439089fa88717b83ff774ee05bdd70bbc
Lisla Fox <lisla@Lislas-MBP.home>
2022-02-01 15:51:41 +0200

    layout for Header&Navbar&Btn

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import s from './Header.module.css';
+import s from './Header.module.scss';
+import styles from '../../styles/common/Btn.module.scss'
 
 type HeaderPropsType = {
     isAuth: boolean
@@ -13,11 +14,11 @@ type HeaderPropsType = {
 function Header(props: HeaderPropsType) {
     return (
         <header className= {s.header}>
-            <img src='https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg'/>
+            {/*<img src='https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg'/>*/}
             <div className={s.loginBlock}>
                 {props.isAuth
-                    ? <div> {props.login} - <button onClick={props.logout}>Log out</button></div>
-                    : <NavLink to={'/login'}>Login</NavLink>}
+                    ? <div className={s.loginText}> {props.login} <button className={styles.btn} onClick={props.logout}>Log Out</button></div>
+                    : <div className={s.loginText}><NavLink to={'/login'}>Login</NavLink></div> }
             </div>
         </header>
     )
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import s from './Navbar.module.css';
-import DialogItem from "../Dialogs/DialogItem/DialogItem";
-import {DialogsType} from "../../Redux/dialogsReducer";
+import s from './Navbar.module.scss';
 
 
-type NavbarType = {
-    // state: Array<DialogsType>
-}
 
-function Navbar(props: NavbarType) {
-    // let friendsElements = props.state.map(
-    //     d => <DialogItem name={d.name} id={d.id} avatar={d.avatar}/>
-    // );
+
+
+function Navbar() {
+
     return (
         <nav className = {s.nav}>
             <div className = {`${s.item}`}>
@@ -24,25 +19,20 @@ function Navbar(props: NavbarType) {
             <div className = {s.item}>
                 <NavLink  to="/users" activeClassName={s.activeLink}>Users</NavLink>
             </div>
-            <div className = {s.item}>
-                <NavLink  to={"/news"} activeClassName={s.activeLink}>News</NavLink>
-            </div>
+            {/*<div className = {s.item}>*/}
+            {/*    <NavLink  to={"/news"} activeClassName={s.activeLink}>News</NavLink>*/}
+            {/*</div>*/}
             <div className = {s.item}>
                 <NavLink to={"/music"} activeClassName={s.activeLink}>Music</NavLink>
             </div>
-            <div className = {s.item}>
-                <NavLink to={"/settings"} activeClassName={s.activeLink}>Settings</NavLink>
-            </div>
-
-            {/*<div className={s.item}>*/}
-            {/*    <h3>Friends</h3>*/}
-            {/*    {friendsElements}*/}
-
+            {/*<div className = {s.item}>*/}
+            {/*    <NavLink to={"/settings"} activeClassName={s.activeLink}>Settings</NavLink>*/}
             {/*</div>*/}
+
         </nav>
     )
 }
-//console.log(s); show what in s
+
 
 export default Navbar;
 
